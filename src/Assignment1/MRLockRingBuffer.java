fb10433c95abf49d17e6a768f8b3ce771405e559
Joehill27 <josephmhill96@gmail.com>
2020-03-13 16:41:20 -0400

    Progress towards MRLock, still need to test usage.

--- src/Assignment1/MRLockRingBuffer.java
+++ src/Assignment1/MRLockRingBuffer.java
@@ -1,7 +1,6 @@
 package Assignment1;
 
 import java.util.Arrays;
-import java.util.BitSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MRLockRingBuffer<T> {
@@ -10,16 +9,130 @@ public class MRLockRingBuffer<T> {
     private int tail;
     private int capacity;
     private Object[] elements;
-    private BitSet[] queue;
-    private int numResources;
 
     public static void main(String[] args) {
-        MRSimpleLock mrSimpleLock = new MRSimpleLock();
+//        MRSimpleLock mrSimpleLock = new MRSimpleLock();
+//
+//        mrSimpleLock.lock(1);
+//        mrSimpleLock.lock(10);
+//        mrSimpleLock.unlock(10);
+//        mrSimpleLock.unlock(1);
+    }
 
-        mrSimpleLock.lock(1);
-        mrSimpleLock.lock(10);
-        mrSimpleLock.unlock(10);
-        mrSimpleLock.unlock(1);
+    static class MRLock {
+        int bufferSize = 2;
+        int maxThreads = 4;
+        int bufferMask;
+        Cell[] mBuffer;
+        AtomicInteger mHead;
+        AtomicInteger mTail;
+
+        MRLock() {
+
+            while(bufferSize <= maxThreads)
+            {
+                bufferSize = bufferSize << 1;
+            }
+            mBuffer = new Cell[bufferSize];
+            bufferMask = bufferSize - 1;
+            for (int  i = 0; i < bufferSize; i++)
+            {
+                mBuffer[i].mSequence.set(i);
+
+
+                //m_bits are initialized to all 1s, and will be set to all 1s when dequeued
+                //This ensure that after a thread equeue a new request but before it set the m_bits to
+                //proper value, the following request will not pass through
+                mBuffer[i].setmBits(~0);
+            }
+            mHead = new AtomicInteger(0);
+            mTail = new AtomicInteger(0);
+        }
+
+        public int lock(final int resources) {
+            Cell cell;
+            int position;
+
+            for(;;) {
+                position = mTail.get();
+                cell = mBuffer[position & bufferMask];
+                int sequence = cell.getmSequence().get();
+                int difference = sequence - position;
+
+                if(difference == 0) {
+                    if (mTail.compareAndSet(position, position + 1)) {
+                        break;
+                    }
+                }
+            }
+            cell.setmBits(resources);
+            cell.setmSequence(position+1);
+
+            //Spin on all previous locks
+            int spinPos = mHead.get();
+            while(spinPos != position)
+            {
+                //We start from the head moving toward my pos, spin on cell that collide with my request
+                //When that cell is freed we move on to the next one util reaching myself
+                //we need to check both m_sequence and m_bits, because either of them could be set to
+                //indicate a free cell, and we want to move on quickly
+                if(position - mBuffer[spinPos & bufferMask].getmSequence().get() > bufferMask
+                        || (mBuffer[spinPos & bufferMask].getmBits() & resources) == 0)
+                {
+                    spinPos++;
+                }
+            }
+
+            //Good to go
+            return position;
+        }
+
+        public void unlock(final int handle) {
+            //Release my lock by setting the bits to 0
+            mBuffer[handle & bufferMask].setmBits(0);
+
+            //Dequeue cells that have been released
+            int position = mHead.get();
+            while(mBuffer[position & bufferMask].getmBits() != 0)
+            {
+                Cell cell = mBuffer[position & bufferMask];
+                int seq = cell.getmSequence().get();
+                int difference = seq - (position + 1);
+
+                if(difference == 0)
+                {
+                    if(mHead.compareAndSet(position, position + 1))
+                    {
+                        cell.setmBits(~0);
+                        cell.setmSequence(position + bufferMask + 1);
+                    }
+                }
+
+                position = mHead.get();
+            }
+
+        }
+    }
+
+    static class Cell {
+        AtomicInteger mSequence;
+        int mBits;
+
+        public AtomicInteger getmSequence() {
+            return mSequence;
+        }
+
+        public void setmSequence(int mSeq) {
+            mSequence.set(mSeq);
+        }
+
+        public int getmBits() {
+            return mBits;
+        }
+
+        public void setmBits(int mBits) {
+            mBits = mBits;
+        }
     }
 
     static class MRSimpleLock {
